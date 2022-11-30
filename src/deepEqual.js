73054fd29161df5a122bc433ccf8f06b723a75ce
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ src/deepEqual.js
@@ -0,0 +1,42 @@
+function hasValidProperties(obj) {
+  for (let key in obj) {
+    const value = obj[key];
+    const type = typeof value;
+    if (
+      type !== "string" &&
+      type !== "number" &&
+      type !== "boolean" &&
+      type !== "undefined" &&
+      type === "object" &&
+      value !== null
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const deepEqual = (a, b) => {
+  // false if object contains other than string/boolean/number
+  if (!hasValidProperties(a) || !hasValidProperties(b)) {
+    console.log("wrong object!");
+    return false;
+  }
+
+  // The objects themselves could be null or undefined.
+  if ((a === undefined || a === null) && a !== b) {
+    return false;
+  }
+
+  const objMerged = { ...a, ...b }; // merge two objects in order to get all possible keys
+  for (let key in objMerged) {
+    if (a[key] !== b[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+module.exports = deepEqual;
